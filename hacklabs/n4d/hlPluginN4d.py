f4f2b8f5624db94e51d5c56de24a7f40f01b8fff
Angel Berlanas Vicente <angel.berlanas@gmail.com>
2011-10-04 16:56:31 +0200

    Fixed configuration file

---
+++ hacklabs/n4d/hlPluginN4d.py
@@ -0,0 +1,16 @@
+#  -*- encoding utf-8 *-*
+
+class Hacklab:
+	
+	def __init__(self):
+		self.text="Angel es DIOS"
+
+	def suma(self, a, b):
+		return a+b
+	
+	def getText(self):
+		return self.text
+	
+	
+#if __name__ 
+	
