be91eb22b1c367337b442ac23a826372639f8727
Angel Berlanas Vicente <angel.berlanas@gmail.com>
2011-12-26 09:14:36 +0100

    Now convert all png to ps before create a simple pdf

--- utils/pdfbooklet.sh
+++ utils/pdfbooklet.sh
@@ -2,20 +2,25 @@
 # Author: Angel Berlanas Vicente
 # This script is licensed under GPL v3 or higher
 
-function _usage()
+# Some temporal values
+TRANSFIGURATION_DIRECTORY="$(mktemp -d /tmp/pdfbooklet.XXXXX)"
+FINAL_PDF="book_output.pdf"
+
+_usage()
 {
-	echo "pdfbooklet.sh FILE_TO_PARSE"
+	echo "pdfbooklet.sh --book FILE_TO_PARSE"
+	echo "pdfbooklet.sh --target FINAL_PDF.pdf --png FILE1.png FILE2.png .."
 	exit 1
 }
 
-function _clean()
+_clean_booklet()
 {
 	echo "Cleaning area"
 	rm "$FILE_TO_PARSE.ps"
 	rm "$FILE_TO_PARSE.A5.ps"
 }
 
-function _booklet()
+_booklet()
 {	
 	FILE_TO_PARSE="$1"
 	pdftops "$FILE_TO_PARSE" "$FILE_TO_PARSE.ps"
@@ -23,12 +28,58 @@ function _booklet()
 	ps2pdf "$FILE_TO_PARSE.A5.ps" "$FILE_TO_PARSE.A5.pdf"
 }
 
-if [  -eq 0 ]; then 
+_process_single_image()
+{
+	echo "Processing $1"
+	convert $2 $TRANSFIGURATION_DIRECTORY/$1.$(basename "$2").ps
+}
+
+_process_images()
+{
+	echo "Processing files"	
+	TOTAL_NUMBER="$#"
+	CURRENT=0
+	for f in $@ ; do
+		CURRENT=$(($CURRENT+1))
+		_process_single_image "$CURRENT.of.$TOTAL_NUMBER" "$f"
+	done	
+}
+
+_process_directory()
+{
+	echo "Process transfiguration directory"
+	gs -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -sOutputFile=$FINAL_PDF "$TRANSFIGURATION_DIRECTORY"/*.ps
+}
+
+_clean_process()
+{
+	echo "Cleaning working area $TRANSFIGURATION_DIRECTORY"
+	rm -rf $TRANSFIGURATION_DIRECTORY
+}
+
+if [ $#  -eq 0 ]; then 
 	_usage
 fi
 
-# Main 
-_clean
-_booklet "$1"
+case "$1" in
+	--book)
+	shift
+	_booklet "$1"
+	_clean_booklet
+	;;
+	--target)
+	shift
+	FINAL_PDF="$1"
+	shift
+	shift
+	_process_images $@
+	_process_directory
+	_clean_process
+	;;
+	*)
+	_usage
+	;;
+esac
 
+# Main 
 exit 0
