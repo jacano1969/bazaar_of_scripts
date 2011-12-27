7bae5107e9949dafdd29ba3a4fed85fc9ef27292
Angel Berlanas Vicente <angel.berlanas@gmail.com>
2011-12-27 09:21:09 +0100

    Working in progress

--- utils/pypdfbooklet.py
+++ utils/pypdfbooklet.py
@@ -8,11 +8,26 @@
 import PythonMagick
 from pyx import *
 import sys
+import argparse
 
-
-
-
-
-# Main 
-for arg in sys.argv:
-	print arg
+def main():
+	
+	# Parser creation with a simple description
+	parser = argparse.ArgumentParser(prog='pypdfbooklet',description='Pdfbooklet python implementation')
+	# Add arguments to parser
+	parser.add_argument('--images', help='list of images', action='store_images')
+	# Created the parser for "--images" command
+	subparsers = parser.add_subparsers(help='List of images')
+	parser_a = subparsers.add_parser('a', help='a help')
+	
+	
+	
+	
+	
+	
+	args = parser.parse_args()
+	print args
+	
+	
+if __name__ == "__main__":
+    main()
