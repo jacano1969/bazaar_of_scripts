aeb7def521f014b6f6642245e232e8df44d9e58f
Angel Berlanas Vicente <angel.berlanas@gmail.com>
2011-12-27 08:20:47 +0100

    Added python script to manipulate images and create pdfs from the top

---
+++ utils/pdfbooklet.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# This script are licensed under GPL 3 or higher
+# Author: Angel Berlanas Vicente
+
+
+
+import PythonMagick
+from pyx import *
+import sys
+
+
+
+
+
+# Main 
+for arg in sys.argv:
+	print arg
