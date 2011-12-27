7e8161278e3041ea5e2e0773e4874d5f3c901c16
Angel Berlanas Vicente <angel.berlanas@gmail.com>
2011-12-27 09:59:52 +0100

    Fixed argparse

--- utils/pypdfbooklet.py
+++ utils/pypdfbooklet.py
@@ -3,8 +3,6 @@
 # This script are licensed under GPL 3 or higher
 # Author: Angel Berlanas Vicente
 
-
-
 import PythonMagick
 from pyx import *
 import sys
@@ -15,18 +13,17 @@ def main():
 	# Parser creation with a simple description
 	parser = argparse.ArgumentParser(prog='pypdfbooklet',description='Pdfbooklet python implementation')
 	# Add arguments to parser
-	parser.add_argument('--images', help='list of images', action='store_images')
-	# Created the parser for "--images" command
-	subparsers = parser.add_subparsers(help='List of images')
-	parser_a = subparsers.add_parser('a', help='a help')
-	
-	
-	
-	
+	parser.add_argument('--japan-order', help='Japan order for images or pages', action='store_true')
+	parser.add_argument('--keep-resolution',help='Keep the original aspect ration and resolution',action='store_true')
+	parser.add_argument('--images', help='list of images',nargs='*' )
+	parser.add_argument('--pdfoutput', help='Pdf generated',nargs=1)
+	parser.add_argument('--pdfinput',help='Pdf to be bookleted', nargs=1)
 	
 	
 	args = parser.parse_args()
 	print args
+	
+	
 	
 	
 if __name__ == "__main__":
