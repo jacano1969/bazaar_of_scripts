7710628efc20b751954fadf525744587cda92956
Angel Berlanas Vicente <angel.berlanas@gmail.com>
2012-01-09 10:05:38 +0100

    WiP

--- utils/flashVideoSaver.py
+++ utils/flashVideoSaver.py
@@ -8,15 +8,33 @@ import argparse
 import subprocess
 import os
 import types
-import ptrace
+import re
+import pickle
+
+def set_re(debug=False):
+	if(debug):
+		print("[DEBUG] Creating regular expression")
+		
+	my_regex=re.compile('flashplayer',re.IGNORECASE)
+	return my_regex
 
 def list_videos(debug=False):
 	
 	if (debug):
 		print("[DEBUG] List Videos")
 	
-	my_processes=subprocess.call (["ps","-aux"])
-	for my_process in myprocesses:
+	my_processes=subprocess.Popen(["ps","aux"],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+	
+	my_regex_to_save=set_re()
+	output = open('data.pkl', 'wb')
+	pickle.dump(my_regex_to_save,output)
+	
+	my_regex=pickle.load('data.pkl	')
+	#for my_process in myprocesses:
+	for process_p in my_processes.stdout:
+		print(my_regex)
+		if my_regex.match(process_p):
+			print(process_p)
 		
 
 
@@ -37,9 +55,13 @@ def main():
 	
 	results = parser.parse_args()
 	
+	
 	# Help
 	if (results.help):
 		parser.print_help()
+	
+	# Load REGULAR_EXPRESSION
+	REGULAR_EXPRESSION=set_re(results.debug)
 	
 	if (results.debug):
 		print("[DEBUG] Debug is enabled")
