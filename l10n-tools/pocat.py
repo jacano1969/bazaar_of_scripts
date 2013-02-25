a82b0736331f4e5e6f33a0366e46b5e69592d4c4
Angel Berlanas Vicente <angel.berlanas@gmail.com>
2013-02-25 20:25:51 +0100

    Added pocat.py

---
+++ l10n-tools/pocat.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#
+# This script is licensed under GPL v3
+# or higher.
+
+import sys
+import potools
+
+potools.debug_msg("Starting")
+
+if len(sys.argv) > 3:
+	compendium_path = sys.argv[1]
+	orig_path = sys.argv[2]
+	dest_path = sys.argv[3]
+	potools.debug_msg("Tenemos los argumentos")
+
+else:
+	print("faltan parametros")
+	print("File1.po File2.po File_dest.po")
+	sys.exit(0)
+
+potools.debug_msg("Creamos el po destino")
+potools.simple_po_creation(dest_path)
+potools.debug_msg("Unimos el po")
+potools.union_po(compendium_path,orig_path, dest_path)
+potools.debug_msg("Comprobamos duplicados")
+potools.test_duplicates(dest_path)
